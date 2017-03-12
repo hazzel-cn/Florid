6784655281b0933238b6282793bc3373299341d5
Haozhe <396960639@qq.com>
2017-03-12 19:04:30 +0800

    Modification

--- florid.py
+++ florid.py
@@ -20,7 +20,7 @@ sys.dont_write_bytecode = True
 
 florid_banner = {
     'version': '0.3.2 dev',
-    'update': '2017-03-08',
+    'update': '2017-03-07',
     'logo': '''
      _____  _            _     _
     |  ___|| | ___  _ __(_) __| |
@@ -109,7 +109,7 @@ def spider_module_call():
 def sqli_module_call():
     import mod.sqli
     mod.sqli.init()
-    mod.sqli.run(common.WebInfo.url)
+    mod.sqli.run()
 
 
 def other_modules_call():
--- mod/sample.py
+++ mod/sample.py
@@ -1,6 +1,8 @@
+import time
+
 def init():
     return (True, 'SUCCESS')
 
 def run(url):
     #time.sleep(3)
-    print 'Fail'
+    print 'Bad'
